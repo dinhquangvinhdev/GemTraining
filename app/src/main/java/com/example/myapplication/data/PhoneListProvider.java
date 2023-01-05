ad2bdd5cecd3f768d141704573c468aa5bf817b0
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-05 14:10:02 +0700

    Create class extent ContentProvider
    create instant of SQLiteHelper in method onCreate

---
+++ app/src/main/java/com/example/myapplication/data/PhoneListProvider.java
@@ -0,0 +1,50 @@
+package com.example.myapplication.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.security.Provider;
+
+public class PhoneListProvider extends ContentProvider {
+    private PhoneListDatabase mDB;
+
+    @Override
+    public boolean onCreate() {
+        //create an instant of SQLiteHelper
+        mDB = new PhoneListDatabase(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}

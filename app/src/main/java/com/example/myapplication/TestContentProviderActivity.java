9d2ed21f38a9c0d6fc51ea5b115c7aed5f704e08
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-05 15:29:56 +0700

    using ContentResolve and Cursor to get Contacts in your phone by query

--- app/src/main/java/com/example/myapplication/TestContentProviderActivity.java
+++ app/src/main/java/com/example/myapplication/TestContentProviderActivity.java
@@ -3,11 +3,14 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.PackageManagerCompat;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -28,6 +31,33 @@ public class TestContentProviderActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //request permission
                 checkPermission();
+
+                /*Create contentResolve to query*/
+                ContentResolver contentResolver = getContentResolver();
+                /*Uri for contentProvider*/
+                Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+                /*Create cursor for get Contact list through query by contentResolver*/
+                Cursor cursor = contentResolver.query(uri , null, null , null , null);
+
+                //check if cursor not receive any column -> this is end of your day
+                if(cursor.getCount() > 0){
+                    String nameContact, numberPhone;
+                    int key_name, key_phone;
+                    //traver the contact list to end
+                    while (cursor.moveToNext()){
+                        //get id column
+                        key_name = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+                        key_phone = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                        //check id column > 0
+                        if(key_name >= 0 && key_phone > 0){
+                            nameContact = cursor.getString(key_name);
+                            numberPhone = cursor.getString(key_phone);
+                            Log.d("bibi" ,"Contact ## Name : " + nameContact + " || Phone : " + numberPhone);
+                        }
+                    }
+                }else{
+                    Log.d("bibi","not receive any contact from your phone");
+                }
             }
         });
     }

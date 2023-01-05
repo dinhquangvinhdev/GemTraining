bf20754f0cccabd774b3ae0f9c667ebd8660f217
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-05 14:01:51 +0700

    Create attribute for query create database
    The database is "phones" and it has three attributes include : _id , phone_number , user

--- app/src/main/java/com/example/myapplication/data/PhoneListDatabase.java
+++ app/src/main/java/com/example/myapplication/data/PhoneListDatabase.java
@@ -9,6 +9,16 @@ public class PhoneListDatabase extends SQLiteOpenHelper {
     private static final int DB_VERSION = 1;
     private static final String DB_NAME = "PhoneListDatabase";
 
+    //attribute for Database schema
+    public static final String PHONE_TABLE_NAME = "phones";
+    public static final String ID = "_id";
+    public static final String COL_NUMBER = "phone_number";
+    public static final String COL_USER = "user";
+    private static final String QUERY_CREATE_TABLE_PHONELIST =
+            "create table " + PHONE_TABLE_NAME + " (" + ID + " integer primary key autoincrement, "
+            + COL_NUMBER + " text not null, " + COL_USER + " text not null);";
+    private static final String DB_SCHEMA = QUERY_CREATE_TABLE_PHONELIST;
+
     public PhoneListDatabase(Context context){
         super(context,DB_NAME , null , DB_VERSION);
     }

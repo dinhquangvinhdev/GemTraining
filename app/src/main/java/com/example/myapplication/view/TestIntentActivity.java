556a8af26410ce83410a0e2940a7c8ba79ddde49
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-06 14:09:27 +0700

    Example basic Intent Explicit
    a intent for opening other activity

--- app/src/main/java/com/example/myapplication/view/TestIntentActivity.java
+++ app/src/main/java/com/example/myapplication/view/TestIntentActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.myapplication.R;
@@ -11,6 +13,7 @@ public class TestIntentActivity extends AppCompatActivity {
 
     private Button btnIntentImplicit;
     private Button btnIntentExplicit;
+    public static String KEY_INTENT_EXPLICIT = "KEY_INTENT_EXPLICIT ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,14 @@ public class TestIntentActivity extends AppCompatActivity {
         btnIntentImplicit = findViewById(R.id.btn_intentImplicit);
         btnIntentExplicit = findViewById(R.id.btn_intentExplicit);
 
-        //do something in here
+        btnIntentExplicit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(TestIntentActivity.this, SplashActivity.class);
+                //use Extra
+                intent.putExtra(KEY_INTENT_EXPLICIT , 10);
+                startActivity(intent);
+            }
+        });
     }
 }

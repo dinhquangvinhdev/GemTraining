cb841c465090299a9ef513669cc3e26a49aa3c7c
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-10 15:54:07 +0700

    update receiver flag for broadcast receiver

--- app/src/main/java/com/example/myapplication/view/TestBroadcastReceiverActivity.java
+++ app/src/main/java/com/example/myapplication/view/TestBroadcastReceiverActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.example.myapplication.R;
@@ -11,16 +14,34 @@ import com.example.myapplication.broadcastReceiver.MBroad;
 public class TestBroadcastReceiverActivity extends AppCompatActivity {
 
     private MBroad mBroad;
+    int receiverFlags = 0;
+    boolean listenToBroadcastFromOtherApps = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_broadcast_receiver);
 
+        //set receiver flag to false for app's broadcast just receive by this app not for other app
+        changeReceiverFlag(false);
+
         //init broadcast
         mBroad = new MBroad();
         IntentFilter filter = new IntentFilter("android.intent.action.AIRPLANE_MODE");
-        registerReceiver(mBroad , filter);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            registerReceiver(mBroad , filter , receiverFlags);
+        }
+    }
+
+    private void changeReceiverFlag(boolean listenToBroadcastFromOtherApps) {
+        //create a boolean to protect app by other app send unprotected  broadcasts to your app
+        if(listenToBroadcastFromOtherApps){
+            //already receive intent
+            receiverFlags = ContextCompat.RECEIVER_EXPORTED;
+        }else{
+            //already to receive intent
+            receiverFlags = ContextCompat.RECEIVER_NOT_EXPORTED;
+        }
     }
 
     @Override

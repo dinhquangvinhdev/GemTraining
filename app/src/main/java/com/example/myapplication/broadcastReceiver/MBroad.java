3d465d0744992b9448d26e6fea5a701d11385ec9
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-10 10:11:51 +0700

    add broadcastRec into manifest and create a class extent BroadcastReceiver

---
+++ app/src/main/java/com/example/myapplication/broadcastReceiver/MBroad.java
@@ -0,0 +1,12 @@
+package com.example.myapplication.broadcastReceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MBroad extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}

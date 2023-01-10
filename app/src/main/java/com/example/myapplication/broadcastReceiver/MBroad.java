d29e5c4d0812420e48ef3e7b3ab5242b3da069d6
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-10 16:37:47 +0700

    Fix comment and code , update func testSendBroadcastNormal
    - the old comments and codes is wrong
    - fun testSendBroadcastNormal is used for send an intent for application which receive action.

--- app/src/main/java/com/example/myapplication/broadcastReceiver/MBroad.java
+++ app/src/main/java/com/example/myapplication/broadcastReceiver/MBroad.java
@@ -3,11 +3,31 @@ package com.example.myapplication.broadcastReceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+/**
+ * This class is use for processing the intent receive by broadcast
+ */
 public class MBroad extends BroadcastReceiver {
+    private int data;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "change airplant", Toast.LENGTH_SHORT ).show();
+        switch (intent.getAction()){
+            case "action.myaction":
+                Log.d("bibi","before get data " + String.valueOf(data));
+                Bundle bundle = intent.getExtras();
+                if(bundle != null){data = bundle.getInt("action");}
+                Log.d("bibi","after get data " + String.valueOf(data));
+                break;
+            case "android.intent.action.AIRPLANE_MODE":
+                Toast.makeText(context, "change airplant", Toast.LENGTH_SHORT ).show();
+                break;
+            default:
+                Log.d("bibi",intent.getAction() + " can not run in this app");
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/view/TestBroadcastReceiverActivity.java
+++ app/src/main/java/com/example/myapplication/view/TestBroadcastReceiverActivity.java
@@ -3,10 +3,15 @@ package com.example.myapplication.view;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.example.myapplication.R;
 import com.example.myapplication.broadcastReceiver.MBroad;
@@ -16,6 +21,7 @@ public class TestBroadcastReceiverActivity extends AppCompatActivity {
     private MBroad mBroad;
     int receiverFlags = 0;
     boolean listenToBroadcastFromOtherApps = false;
+    private int data = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,26 +29,35 @@ public class TestBroadcastReceiverActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test_broadcast_receiver);
 
         //set receiver flag to false for app's broadcast just receive by this app not for other app
-        changeReceiverFlag(false);
+        // it need implement androidx library but library required version android 33 ## LOL
+//        changeReceiverFlag(false);
 
-        //init broadcast
+        //init broadcast dynamic
         mBroad = new MBroad();
         IntentFilter filter = new IntentFilter("android.intent.action.AIRPLANE_MODE");
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            registerReceiver(mBroad , filter , receiverFlags);
-        }
+        filter.addAction("action.myaction");
+        registerReceiver(mBroad , filter);
+
+        //normal test send broad cast normal
+        testSendBroadcastNormal();
     }
 
-    private void changeReceiverFlag(boolean listenToBroadcastFromOtherApps) {
-        //create a boolean to protect app by other app send unprotected  broadcasts to your app
-        if(listenToBroadcastFromOtherApps){
-            //already receive intent
-            receiverFlags = ContextCompat.RECEIVER_EXPORTED;
-        }else{
-            //already to receive intent
-            receiverFlags = ContextCompat.RECEIVER_NOT_EXPORTED;
-        }
+    private void testSendBroadcastNormal() {
+        Intent intent = new Intent("action.myaction");
+        intent.putExtra("action",1);
+        sendBroadcast(intent);
     }
+
+//    private void changeReceiverFlag(boolean listenToBroadcastFromOtherApps) {
+//        //create a boolean to protect app by other app send unprotected  broadcasts to your app
+//        if(listenToBroadcastFromOtherApps){
+//            //already receive intent
+//            receiverFlags = ContextCompat.RECEIVER_EXPORTED;
+//        }else{
+//            //already to receive intent
+//            receiverFlags = ContextCompat.RECEIVER_NOT_EXPORTED;
+//        }
+//    }
 
     @Override
     protected void onStop() {

915f86a57f919cfe56a5babe3d0a9f6dfe67a145
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-10 10:25:47 +0700

    register broadcastRec in java file
    Toast notification if change air plant

--- app/src/main/java/com/example/myapplication/broadcastReceiver/MBroad.java
+++ app/src/main/java/com/example/myapplication/broadcastReceiver/MBroad.java
@@ -3,10 +3,11 @@ package com.example.myapplication.broadcastReceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MBroad extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Toast.makeText(context, "change airplant", Toast.LENGTH_SHORT ).show();
     }
 }
--- app/src/main/java/com/example/myapplication/view/TestBroadcastReceiverActivity.java
+++ app/src/main/java/com/example/myapplication/view/TestBroadcastReceiverActivity.java
@@ -2,17 +2,30 @@ package com.example.myapplication.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 import com.example.myapplication.R;
+import com.example.myapplication.broadcastReceiver.MBroad;
 
 public class TestBroadcastReceiverActivity extends AppCompatActivity {
+
+    private MBroad mBroad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_broadcast_receiver);
 
+        //init broadcast
+        mBroad = new MBroad();
+        IntentFilter filter = new IntentFilter("android.intent.action.AIRPLANE_MODE");
+        registerReceiver(mBroad , filter);
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(mBroad);
     }
 }

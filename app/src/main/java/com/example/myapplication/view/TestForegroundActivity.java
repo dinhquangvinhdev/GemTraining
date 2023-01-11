dc276617f66efa9ba4f8c130162c027d7a6f6c7d
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-11 14:26:45 +0700

    Create a new activity , channel notification

---
+++ app/src/main/java/com/example/myapplication/view/TestForegroundActivity.java
@@ -0,0 +1,38 @@
+package com.example.myapplication.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.example.myapplication.R;
+
+public class TestForegroundActivity extends AppCompatActivity {
+
+    public static final String CHANNEL_ID = "exampleServiceChannel";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_foreground);
+
+        createChannelNotification();
+
+    }
+
+    private void createChannelNotification() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            //create notificationChannel
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "This is my test notification channel",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            //create notification manager to run it
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+}

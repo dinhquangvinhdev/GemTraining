28c3332be05c8c312c42b6a45fce025e5872b3b1
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-12 19:42:07 +0700

    Update log and logic of MedialPlayer

--- app/src/main/java/com/example/myapplication/service/MBoundService.java
+++ app/src/main/java/com/example/myapplication/service/MBoundService.java
@@ -6,14 +6,30 @@ import android.content.ServiceConnection;
 import android.media.MediaPlayer;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
 import com.example.myapplication.R;
 
+import java.io.IOException;
+
 public class MBoundService extends Service {
     private final IBinder mBinder = new LocalBinder();
-    private final MediaPlayer player = MediaPlayer.create(this , R.raw.drum);
+    private MediaPlayer player;
+
+    @Override
+    public void onCreate() {
+        Log.d("bibi", "running onCreate of Service");
+        player = MediaPlayer.create(this , R.raw.drum);
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("bibi", "running onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
 
     public class LocalBinder extends Binder{
         public MBoundService getService(){
@@ -24,7 +40,19 @@ public class MBoundService extends Service {
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        Log.d("bibi", "running onBind of Service");
         return mBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d("bibi", "running onDestroy of Service");
+        if(player != null){
+            player.stop();
+            player.release();
+            player = null;
+        }
     }
 
     public void playMusic(){
@@ -33,7 +61,14 @@ public class MBoundService extends Service {
     }
 
     public void stopMusic(){
-        if(player.isPlaying())
+        if(player.isPlaying()){
             player.stop();
+            try {
+                player.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }

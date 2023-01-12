9767715c2fbcaa498b5bdc62d5ea620ea2d92c0c
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-12 18:57:53 +0700

    create an extend class Binder , public method control , get instant Service
    - Create instant Binder has public method which client can call
    - Return instant of Service with public method which client can call
    - Returns an instance of another class managed by the service with public methods that the client can call.

--- app/src/main/java/com/example/myapplication/service/MBoundService.java
+++ app/src/main/java/com/example/myapplication/service/MBoundService.java
@@ -2,14 +2,37 @@ package com.example.myapplication.service;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Binder;
 import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
+import com.example.myapplication.R;
+
 public class MBoundService extends Service {
+    private final IBinder mBinder = new LocalBinder();
+    private final MediaPlayer player = MediaPlayer.create(this , R.raw.drum);
+
+    public class LocalBinder extends Binder{
+        LocalBinder getService(){
+            return LocalBinder.this;
+        }
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mBinder;
+    }
+
+    public void playMusic(){
+        if(!player.isPlaying())
+            player.start();
+    }
+
+    public void stopMusic(){
+        if(player.isPlaying())
+            player.stop();
     }
 }

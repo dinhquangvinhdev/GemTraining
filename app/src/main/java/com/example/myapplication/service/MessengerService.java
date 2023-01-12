195439c4101f9a16537a32ea071d2ade608f226a
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-12 20:39:29 +0700

    Create a Service which handles with each messengers
    -Create a class extend service
    -Create a static class extend Handler
    - Create method to process each messenger service has from client in method handleMessage()
    - Create a new Messenger when component bind to service
    - return binder of messenger in method onBind()

---
+++ app/src/main/java/com/example/myapplication/service/MessengerService.java
@@ -0,0 +1,82 @@
+package com.example.myapplication.service;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.myapplication.R;
+
+import java.io.IOException;
+
+public class MessengerService extends Service {
+    static final int MSG_PLAY_MUSIC = 1;
+    static final int MSG_STOP_MUSIC = 2;
+    static final int MSG_PAUSE_MUSIC = 3;
+
+    static class IncomingHandler extends Handler{
+        private Context applicationContext;
+        private MediaPlayer player;
+
+        IncomingHandler (Context context){
+            applicationContext = context.getApplicationContext();
+            player = MediaPlayer.create(context , R.raw.drum);
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            switch (msg.what){
+                case MSG_PLAY_MUSIC:
+                    playMusic();
+                    break;
+                case  MSG_PAUSE_MUSIC:
+                    pauseMusic();
+                    break;
+                case MSG_STOP_MUSIC:
+                    stopMusic();
+                    break;
+                default:
+                    super .handleMessage(msg);
+            }
+        }
+
+        public void playMusic(){
+            if (player!=null && !player.isPlaying())
+                player.start();
+        }
+
+        public void pauseMusic(){
+            if(player!=null && player.isPlaying())
+                player.pause();
+        }
+
+        public void stopMusic(){
+            if(player!= null && player.isPlaying()){
+                player.stop();
+                try {
+                    player.prepare();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private Messenger messenger;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        Log.d("bibi", "call onBind create a new Messenger");
+        messenger = new Messenger(new IncomingHandler(this));
+        return messenger.getBinder();
+    }
+}

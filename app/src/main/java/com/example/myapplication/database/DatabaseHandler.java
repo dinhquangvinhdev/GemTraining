9d00767b22e7b8e8c283e5f3e9dee02ef05cf864
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-01-31 16:41:02 +0700

    Update DatabaseHandler.java

--- app/src/main/java/com/example/myapplication/database/DatabaseHandler.java
+++ app/src/main/java/com/example/myapplication/database/DatabaseHandler.java
@@ -1,38 +1,78 @@
 package com.example.myapplication.database;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import com.example.myapplication.model.Customer;
+
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME = "customer_db";
     private static final String TABLE_NAME = "CUSTOMER";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_AGE = "age";
+
     private static final int CUSTOMER_AGE = 10;
     private static final String CUSTOMER_NAME = "Some One";
 
 
-    public DatabaseHandler(@Nullable Context context, @Nullable String name,
-                           @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    public DatabaseHandler(@Nullable Context context, int version) {
+        super(context, DATABASE_NAME, null, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         Log.d("bibi", "create a new database");
-        String createCustomer = String.format("CREATE TABLE %s(%s INTEGER PRIMARY, %s TEXT)"
+        String createCustomer = String.format("CREATE TABLE %s(%s INTEGER PRIMARY KEY,%s TEXT)"
                 , TABLE_NAME
-                , CUSTOMER_AGE
-                , CUSTOMER_NAME);
-        // execl the command SQL
+                , KEY_AGE
+                , KEY_NAME);
+        // exec the command SQL
         sqLiteDatabase.execSQL(createCustomer);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         Log.d("bibi", "update customer database");
+    }
+
+    public void addCustomer(Customer customer){
+        //using SQLiteDatabase for have more instance
+        SQLiteDatabase db = this.getReadableDatabase();
+        //using contentValues for update data for each row
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, customer.getName());
+        values.put(KEY_AGE, customer.getAge());
+        db.insert(TABLE_NAME, null, values);
+        //remember for closing
+        db.close();
+    }
+
+    /**
+     * This func will return null if not find any customer in database
+     * @param name
+     * @return
+     */
+    public Customer queryACustomer(String name){
+        SQLiteDatabase db = this.getReadableDatabase();
+        //create a cursor for read the db
+        Cursor cursor = db.query(TABLE_NAME , null , KEY_NAME + " = ?",
+                new String[]{name}, null , null, null);
+        if(cursor != null && cursor.getCount() > 0){
+            cursor.moveToFirst();
+            Customer customer = new Customer(cursor.getInt(0), cursor.getString(1));
+            return customer;
+        } else {
+            Log.d("bibi", "not found any customer");
+        }
+        return null;
     }
 }

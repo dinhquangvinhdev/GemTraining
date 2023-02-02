48495651904d342c2ef9c1e2d696718eee50d816
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-02-02 11:12:43 +0700

    add post method in api

--- app/src/main/java/com/example/myapplication/api/MApi.java
+++ app/src/main/java/com/example/myapplication/api/MApi.java
@@ -1,13 +1,19 @@
 package com.example.myapplication.api;
 
 import com.example.myapplication.model.New;
+import com.example.myapplication.model.User;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface MApi {
     @GET("posts")
     Call<List<New>> getData();
+
+    @POST("posts")
+    Call<New> createNew(@Body New aNew);
 }

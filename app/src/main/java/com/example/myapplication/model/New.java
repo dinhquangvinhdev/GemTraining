47a2a62c7cea053e6fe3479242320922bb27e70a
dinhquangvinhdev <121219528+dinhquangvinhdev@users.noreply.github.com>
2023-02-02 11:12:13 +0700

    Update model New and update xml dialog add new

--- app/src/main/java/com/example/myapplication/model/New.java
+++ app/src/main/java/com/example/myapplication/model/New.java
@@ -1,14 +1,34 @@
 package com.example.myapplication.model;
 
+import com.google.gson.annotations.SerializedName;
+
 public class New {
+    @SerializedName("userId")
+    int idUser;
     int id;
     String title;
     String body;
 
-    public New(int id , String title , String body){
+    public New(int idUser, int id, String title, String body) {
+        this.idUser = idUser;
         this.id = id;
         this.title = title;
         this.body = body;
+    }
+
+    public New(int id , String title , String body){
+        this.idUser = 1;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
     }
 
     public int getId() {
